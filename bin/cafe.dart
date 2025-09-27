4dd37e8d29e6c57cb9b85837250c08719319b00f
pastel67 <chykjoun@gmail.com>
2025-09-27 13:15:09 +0900

    메뉴 위치 변경 , 주문방식 변경,  카운터직원 클래스 생성 및 카운터 상속,  주문 받는 부분과 노티하는 멘트 매서드 분리

--- bin/cafe.dart
+++ bin/cafe.dart
@@ -1,44 +1,54 @@
+Map<String, int> menu = {
+  '아메리카노': 3000,
+  '카페라떼': 4500,
+  '카페모카': 4800,
+  '카라멜라떼': 5000,
+  '바닐라라떼': 5000,
+  '딸기스무디': 6000,
+  '블루베리스무디': 6000,
+  '플레인스무디': 6000,
+};
+
 void main() {
-  print('메뉴 번호로 주문해주세요.');
-  var customer1 = Counter('${menu[9]}', true, true);
+  var customer1 = counterStaff('아메리카노', true, true);
   customer1.Order();
 }
 
-Map<int, String> menu = {
-  1: '아메리카노',
-  2: '카페라떼',
-  3: '카페모카',
-  4: '카라멜라떼',
-  5: '바닐라라떼',
-  6: '딸기스무디',
-  7: '블루베리스무디',
-  8: '플레인스무디',
-};
-
-class Counter {
+class Counter{
   String beverage = '';
   bool ice;
   bool takeOut;
 
   Counter(this.beverage, this.ice, this.takeOut);
 
+}
+
+
+class counterStaff extends Counter{
+
+  String takeOrder = '';
+  int totalPrice = 0;
+
+  counterStaff(super.beverage, super.ice, super.takeOut);
+
+  @override
   void Order() {
-    String takeOrder = '';
 
     if (ice == true) {
-      takeOrder += '아이스 ';
+     takeOrder += '아이스 ';
     }
-    if(beverage == 'null'){
-      print('다시 주문해 주세요.');
-      return;
-    }else{
-      takeOrder += beverage;
-    }
+  takeOrder += beverage;
+  totalPrice += menu[beverage]?? 0;
 
+  noti();
+  }
+
+  @override
+  void noti(){ 
     if (takeOut == true) {
-      print('$takeOrder 주문받았습니다. 일회용컵으로 준비해드릴게요.');
+      print('$takeOrder 주문받았습니다.\n$totalPrice원입니다.\n일회용컵으로 준비해드릴게요.');
     } else if (takeOut == false) {
-      print('$takeOrder 주문받았습니다. 매장컵으로 준비해드릴게요.');
+      print('$takeOrder 주문받았습니다.\n$totalPrice원입니다.\n매장컵으로 준비해드릴게요.');
     } else {
       print('$takeOrder 주문받았습니다. 드시고 가시나요?');
     }
