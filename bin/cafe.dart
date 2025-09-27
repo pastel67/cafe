d840633df48621a1a46eccebb19fc48d049b3598
pastel67 <chykjoun@gmail.com>
2025-09-27 11:55:53 +0900

    메뉴 맵으로 변경, 클래스명 수정, 주문메뉴입력 방식 수정

--- bin/cafe.dart
+++ bin/cafe.dart
@@ -1,37 +1,38 @@
 void main() {
-  var customer1 = PosStaff('아메리카노', true, true);
+  print('메뉴 번호로 주문해주세요.');
+  var customer1 = Counter('${menu[3]}', true, true);
   customer1.Order();
 }
 
-List<String> menu = [
-  '아메리카노',
-  '카페라떼',
-  '카페모카',
-  '카라멜라떼',
-  '바닐라라떼',
-  '딸기스무디',
-  '블루베리스무디',
-  '플레인스무디',
-];
+Map<int, String> menu = {
+  1: '아메리카노',
+  2: '카페라떼',
+  3: '카페모카',
+  4: '카라멜라떼',
+  5: '바닐라라떼',
+  6: '딸기스무디',
+  7: '블루베리스무디',
+  8: '플레인스무디',
+};
 
-class PosStaff {
+class Counter {
   String beverage = '';
   bool ice;
   bool takeOut;
 
-  PosStaff(this.beverage, this.ice, this.takeOut);
+  Counter(this.beverage, this.ice, this.takeOut);
 
   void Order() {
     String takeOrder = '';
 
     if (ice == true) {
-      takeOrder += '아이스';
+      takeOrder += '아이스 ';
     }
 
     takeOrder += beverage;
 
     if (takeOut == true) {
-      print('$takeOrder 주문받았습니다. 일회용컵에 준비해드릴게요.');
+      print('$takeOrder 주문받았습니다. 일회용컵으로 준비해드릴게요.');
     } else if (takeOut == false) {
       print('$takeOrder 주문받았습니다. 매장컵으로 준비해드릴게요.');
     } else {
