42365d9022774f3fc0d8d0134d8973e2ecbcd93f
pastel67 <chykjoun@gmail.com>
2025-09-27 11:58:46 +0900

    beverage가 null이 나올 수 있는 상황 대처 추가

--- bin/cafe.dart
+++ bin/cafe.dart
@@ -1,6 +1,6 @@
 void main() {
   print('메뉴 번호로 주문해주세요.');
-  var customer1 = Counter('${menu[3]}', true, true);
+  var customer1 = Counter('${menu[9]}', true, true);
   customer1.Order();
 }
 
@@ -28,8 +28,11 @@ class Counter {
     if (ice == true) {
       takeOrder += '아이스 ';
     }
-
-    takeOrder += beverage;
+    if(beverage == null){
+      print('다시 주문해 주세요.');
+    }else{
+      takeOrder += beverage;
+    }
 
     if (takeOut == true) {
       print('$takeOrder 주문받았습니다. 일회용컵으로 준비해드릴게요.');
